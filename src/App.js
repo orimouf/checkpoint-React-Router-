4502758045df9568891446e69ab45f574023c301
orimouf <orim.ouf@gmail.com>
2021-05-05 21:59:01 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import './App.css';
 import MovieCard from './Components/MovieCard'
 import Search from './Components/Search'
 
-const Data = [{
-  "title": "V for Vandetta",
-  "poster": "http://i.ebayimg.com/00/s/NTAwWDMzMw==/z/VIsAAOxyaTxTWIqs/$_3.JPG?set_id=2",
-  "rating": 5
-},
-{
-  "title": "The Detachement",
-  "poster": "https://www.joblo.com/assets/images/oldsite/posters/images/full/detachment-french-poster.jpg",
-  "rating": 4
-},
-{
-  "title": "The experiment",
-  "poster": "https://images-na.ssl-images-amazon.com/images/I/51UFOnvEviL.jpg",
-  "rating": 5
-},
-{
-  "title": "Inception",
-  "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-  "rating": 4
-}
+const Data = [
+  {
+    "title": "V for Vandetta",
+    "poster": "http://i.ebayimg.com/00/s/NTAwWDMzMw==/z/VIsAAOxyaTxTWIqs/$_3.JPG?set_id=2",
+    "rating": 5,
+    "description": "In a futuristic, totalitarian Britain, a freedom fighter known simply as V, uses terrorist tactics to fight the oppressive society. Evey aids him in his mission to bring down the government."
+  },
+  {
+    "title": "The Detachement",
+    "poster": "https://www.joblo.com/assets/images/oldsite/posters/images/full/detachment-french-poster.jpg",
+    "rating": 4,
+    "description": "Henry Barthes (Adrien Brody) is a substitute teacher who shuns emotional connections, and never stays long enough in one district to bond with his students or colleagues. "
+  },
+  {
+    "title": "The experiment",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/51UFOnvEviL.jpg",
+    "rating": 5,
+    "description": "Twenty-six subjects are chosen to participate in a psychological experiment. While one group plays the role of prison guards, the others play inmates. Can the guards maintain order when mutiny erupts?"
+  },
+  {
+    "title": "Inception",
+    "poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+    "rating": 4,
+    "description": "Initiation or Transplant is a 2010 science fiction movie written, directed and produced by Christopher Nolan. The film stars Leonardo DiCaprio, Ken Watanabe, Joseph Gordon-Levitt, Marion Cotillard, Ellen Page, Cillian Murphy, Tom Hardy, Delibi Rao, Tom Bringer and Michael Kane."
+  }
 ];
 
 function App() {
